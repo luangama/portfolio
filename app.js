e39896199760d023512382031dd13a64ca874bc6
luan gama <luanmsgama@gmail.com>
2023-08-28 01:04:51 -0300

    changes in js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let data = new Date();
 let hora = data.getHours();
 let boasVindas = document.getElementById("teste");
-let mensagens = ['Bom dia,', 'Bom tarde,', 'Boa noite,'];
+let mensagens = ['Bom dia, seja bem-vindo(a)!', 'Boa tarde, seja bem-vindo(a)!', 'Boa noite, seja bem-vindo(a)!'];
 
 if (hora > 6 && hora < 12) {
-    boasVindas.innerHTML = "<h1>" + mensagens[0] + "<h1>" + "<h1>" + '&nbsp;seja bem-vindo(a)!' + "<h1>";
+    boasVindas.innerHTML = "<h1>" + mensagens[0] + "<h1>";
 } else if (hora > 12 && hora < 18) {
-    boasVindas.innerHTML = "<h1>" + mensagens[1] + "<h1>" + "<h1>" + '&nbsp;seja bem-vindo(a)!' + "<h1>";
+    boasVindas.innerHTML = "<h1>" + mensagens[1] + "<h1>";
 } else {
-    boasVindas.innerHTML = "<h1>" + mensagens[2] + "<h1>" + "<h1>" + '&nbsp;seja bem-vindo(a)!' + "<h1>";
+    boasVindas.innerHTML = "<h1>" + mensagens[2] + "<h1>";
 }
